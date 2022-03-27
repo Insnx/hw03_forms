bbcfb029a6558ac2546124396095c677cd0b04e0
Valentin Bobylev <itspri@yandex.ru>
2022-03-27 08:59:30 -0500

    внес правки

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -5,6 +5,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        text = forms.CharField(required=True)
-        group = forms.CharField(required=False)
         fields = ('text', 'group')
